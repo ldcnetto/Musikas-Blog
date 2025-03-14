cdae2952f2287620e965d512c500d2e4e9acfd33
ldcnetto <ldcn@cin.ufpe.br>
2025-03-13 21:54:00 -0300

    refactor: incluindo novos imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MenuBarComponent} from './components/menu-bar/menu-bar.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MainBannerComponent } from './components/banner/main-banner.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { BackgroundComponent } from './components/background/background.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuBarComponent],
+  imports: [RouterOutlet, MenuBarComponent, LayoutComponent, BackgroundComponent, MainBannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-blog';
+  title = 'Musikas';
 }
