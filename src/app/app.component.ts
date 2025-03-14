6758e0f9f4fcea5340a4c951396ba9e275a6ebae
ldcnetto <ldcn@cin.ufpe.br>
2025-03-14 14:27:23 -0300

    refactor: atualizando rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet, Routes} from '@angular/router';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MainBannerComponent } from './components/banner/main-banner.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { BackgroundComponent } from './components/background/background.component';
+import {CardComponent} from './components/card/card.component';
+import {CardGridComponent} from './components/card-grid/card-grid.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuBarComponent, LayoutComponent, BackgroundComponent, MainBannerComponent],
+  imports: [RouterOutlet, MenuBarComponent, LayoutComponent, BackgroundComponent, MainBannerComponent, CardComponent, CardGridComponent, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
